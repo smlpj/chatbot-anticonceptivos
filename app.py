e864f62327a0ab05a97ef56618c520d1da7dbb32
smlpj <slopeza@uninorte.edu.co>
2022-11-24 10:49:04 -0500

    Requirements modified and Flask_cors added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 from chat import get_response
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.get("/")
@@ -19,4 +21,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
